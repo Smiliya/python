821083ef6199b6caa26f66a4d54780a5b38ed37b
Smiliya sathiyadass S <smiliyasmiliya9944@gmail.com>
2026-02-19 14:28:42 +0530

    Add files via upload

---
+++ cylinder.py
@@ -0,0 +1,11 @@
+import math
+
+# Taking input from user
+radius = float(input("Enter the radius of the cylinder: "))
+height = float(input("Enter the height of the cylinder: "))
+
+# Calculating volume
+volume = math.pi * radius**2 * height
+
+# Display result
+print("Volume of the cylinder is:", volume)
